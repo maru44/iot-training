25218848ad7ce6dc9265cecee3b33f535d2254b1
maru44 <takumaru22244@gmail.com>
2023-12-12 12:42:32 +0900

    GPS OK

--- 0010_gps/src/main.cpp
+++ 0010_gps/src/main.cpp
@@ -1,172 +1,125 @@
-// ref: https://github.com/Xinyuan-LilyGO/LilyGo-T-PCIE/blob/master/examples/SIM7080/Platformio/src/main.cpp
-#define TINY_GSM_MODEM_SIM7080
-
+#include <Arduino.h>
+#include <cstdlib>
 #include "HardwareSerial.h"
 
-// // See all AT commands, if wanted 如果需要，请查看所有AT命令
-// #define DUMP_AT_COMMANDS
+#define VOLT_PIN 32
+#define HALL_GPIO GPIO_NUM_34
+#define SIM_PWR_PIN 22
+#define SIM_KEY_PIN 21
 
-// Define the serial console for debug prints, if needed 如果需要，为调试打印定义串行控制台
-#define TINY_GSM_DEBUG Serial
+#define DBGSERIAL
 
-/*
-   Tests enabled
-*/
-#define TINY_GSM_TEST_GPRS true
-#define TINY_GSM_TEST_WIFI false
-#define TINY_GSM_TEST_CALL false
-#define TINY_GSM_TEST_SMS false
-#define TINY_GSM_TEST_USSD false
-#define TINY_GSM_TEST_BATTERY false
-#define TINY_GSM_TEST_GPS true
-// powerdown modem after tests
-#define TINY_GSM_POWERDOWN true
+#define SLEEP_TIME 1 * 60 * 1000000ull // sleep time (1min)
 
-// set GSM PIN, if any 如果有GSM PIN，请设置
-#define GSM_PIN ""
+#define TINY_GSM_MODEM_SIM7080
+#include <TinyGSM.h>
+// TinyGsm modem(Serial2);
 
-// Your GPRS credentials, if any
-const char apn[] = "soracom.io";
-const char gprsUser[] = "sora";
-const char gprsPass[] = "sora";
-
-#include <TinyGsmClient.h>
-
-#ifdef DUMP_AT_COMMANDS
 #include <StreamDebugger.h>
 StreamDebugger debugger(Serial2, Serial);
 TinyGsm modem(debugger);
-#else
-TinyGsm modem(Serial2);
+
+void startSleep()
+{
+    modem.poweroff();
+#ifdef DBGSERIAL
+    Serial.println("sleep");
 #endif
+    delay(10);
+    digitalWrite(SIM_PWR_PIN, LOW);
+    esp_sleep_enable_timer_wakeup(SLEEP_TIME);
+    delay(50);
+    esp_deep_sleep_start();
+}
 
-#define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP 60          /*  Time ESP32 will go to sleep (in seconds) */
+void connectGPS()
+{
+    // pull SIM7080G(BK)'s K pin to ground
+    digitalWrite(SIM_KEY_PIN, LOW);
+    delay(1000);
+    digitalWrite(SIM_KEY_PIN, HIGH);
 
-#define UART_BAUD 115200
+    if (!modem.init())
+    {
+#ifdef DBGSERIAL
+        Serial.println("failed to init modem");
+        Serial.println("sleep");
+#endif
+        delay(10);
+        digitalWrite(SIM_PWR_PIN, LOW);
+        esp_sleep_enable_timer_wakeup(SLEEP_TIME);
+        delay(50);
+        esp_deep_sleep_start();
+        return;
+    }
 
-#define PIN_SIM 25
+    if (!modem.enableGPS())
+    {
+#ifdef DBGSERIAL
+        Serial.println("failed to enable GPS");
+#endif
+        startSleep();
+    }
+}
 
 void setup()
 {
-    // Set console baud rate
+#ifdef DBGSERIAL
     Serial.begin(115200);
-    delay(10);
+#endif
+    pinMode(VOLT_PIN, ANALOG);
+    pinMode(SIM_PWR_PIN, OUTPUT);
+    pinMode(SIM_KEY_PIN, OUTPUT);
 
-    Serial2.begin(UART_BAUD);
-    delay(100);
+    Serial2.begin(115200);
+    digitalWrite(SIM_KEY_PIN, HIGH);
+    digitalWrite(SIM_PWR_PIN, HIGH);
+    delay(20);
 }
 
 void loop()
 {
-    // Restart takes quite some time
-    // To skip it, call init() instead of restart()
-    DBG("Initializing modem...");
-    if (!modem.init())
+    esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
+
+    connectGPS();
+
+    float lat2 = 0;
+    float lon2 = 0;
+    float speed2 = 0;
+    float alt2 = 0;
+    int vsat2 = 0;
+    int usat2 = 0;
+    float accuracy2 = 0;
+    int year2 = 0;
+    int month2 = 0;
+    int day2 = 0;
+    int hour2 = 0;
+    int min2 = 0;
+    int sec2 = 0;
+    for (int8_t i = 15; i; i--)
     {
-        DBG("Failed to restart modem, delaying 10s and retrying");
-        return;
-    }
-
-    String name = modem.getModemName();
-    DBG("Modem Name:", name);
-
-    String modemInfo = modem.getModemInfo();
-    DBG("Modem Info:", modemInfo);
-
-    DBG("Waiting for network...");
-    if (!modem.waitForNetwork())
-    {
-        delay(10000);
-        return;
-    }
-
-    if (modem.isNetworkConnected())
-    {
-        DBG("Network connected");
-    }
-
-#if TINY_GSM_TEST_GPRS
-    DBG("Connecting to", apn);
-    if (!modem.gprsConnect(apn, gprsUser, gprsPass))
-    {
-        delay(10000);
-        return;
-    }
-
-    bool res = modem.isGprsConnected();
-    DBG("GPRS status:", res ? "connected" : "not connected");
-
-    String ccid = modem.getSimCCID();
-    DBG("CCID:", ccid);
-
-    String imei = modem.getIMEI();
-    DBG("IMEI:", imei);
-
-    String cop = modem.getOperator();
-    DBG("Operator:", cop);
-
-    IPAddress local = modem.localIP();
-    DBG("Local IP:", local);
-
-    int csq = modem.getSignalQuality();
-    DBG("Signal quality:", csq);
-
-#endif
-
-#if TINY_GSM_TEST_GPS
-    modem.disableGPS();
-    float lat, lon;
-
-    while (1)
-    {
-        if (!modem.enableGPS())
+        delay(5000L);
+        modem.sendAT("+CGNSINF");
+        if (modem.getGPS(&lat2, &lon2, &speed2, &alt2, &vsat2, &usat2, &accuracy2,
+                         &year2, &month2, &day2, &hour2, &min2, &sec2))
         {
-            Serial.println("gps not enabled");
-            delay(200);
-            continue;
-        }
-        if (modem.getGPS(&lat, &lon))
-        {
-            Serial.printf("lat:%f lon:%f\n", lat, lon);
-            // tick.attach_ms(200, []()
-            //                { digitalWrite(LED_PIN, !digitalRead(LED_PIN)); });
+            Serial.printf("lat %g\n", lat2);
+            Serial.printf("lon %g\n", lon2);
+
+            String gps_str = modem.getGPSraw();
+            const char *raw = NULL;
+            raw = gps_str.c_str();
+            Serial.printf("raw %s\n", raw);
             break;
         }
         else
         {
-            Serial.print("getGPS ");
-            Serial.println(millis());
+            Serial.printf("failed to get GPS: %d\n", i);
         }
-        delay(2000);
     }
     modem.disableGPS();
 
-    modem.sendAT("+CGPIO=0,58,0,0");
-    modem.sendAT("+CGPIO=0,57,0,0");
-#endif
+    Serial.println("loop finished");
 
-#if TINY_GSM_TEST_GPRS
-    modem.gprsDisconnect();
-    if (!modem.isGprsConnected())
-    {
-        DBG("GPRS disconnected");
-    }
-    else
-    {
-        DBG("GPRS disconnect: Failed.");
-    }
-#endif
-
-#if TINY_GSM_POWERDOWN
-    // Try to power-off (modem may decide to restart automatically)
-    // To turn off modem completely, please use Reset/Enable pins
-    modem.poweroff();
-    DBG("Poweroff.");
-#endif
-
-    // Test is complete Set it to sleep mode
-    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-    delay(200);
-    esp_deep_sleep_start();
+    startSleep();
 }
